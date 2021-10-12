02d0227b50ffde0cdbfdc6a376d9ce73a3827bcf
priemum <50713962+priemum@users.noreply.github.com>
2021-10-12 23:13:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 module.exports = class HtmlDB {
   constructor(path) {
     this.path = path;
-  }
+  }   
 
   get raw() {
     return fs.readFileSync(this.path, 'utf-8');
@@ -29,7 +29,7 @@ module.exports = class HtmlDB {
     if(!this.raw.match(regex)) return null;
     return this.raw.match(regex)[0].replace(regex, '$1');
   }
-
+   
   has(key) {
     return this.keys().includes(key);
   }
